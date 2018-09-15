d21a03c325f2c56ababfa6de87626bac8bc3e438
DONGVAN <viendong194@gmail.com>
2018-09-15 12:58:58 +0900

    add bootstrap and layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './layout/Navbar';
+import Footer from './layout/Footer';
+import Landing from './layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
