9a51e710ecb299824db8864bed8ba485b704ca95
DONGVAN <viendong194@gmail.com>
2018-09-15 15:06:09 +0900

    add login and register

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Landing extends Component {
+class Landing extends Component {
   render() {
     return (
       <div className="landing">
@@ -15,12 +16,12 @@ export default class Landing extends Component {
                   from other developers
                 </p>
                 <hr />
-                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
                   Sign Up
-                </a>
-                <a href="login.html" className="btn btn-lg btn-light">
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
                   Login
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -29,3 +30,5 @@ export default class Landing extends Component {
     );
   }
 }
+
+export default Landing;
