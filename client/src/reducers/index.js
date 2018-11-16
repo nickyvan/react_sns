4f8994ac1b0f29659473e0109f5873977f7120a8
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-11-16 17:47:50 +0900

    add profile reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducers from './authReducers';
 import errorReducers from './errorReducers';
-
+import profileReducers from './profileReducers';
 export default combineReducers({
   auth: authReducers,
-  errors: errorReducers
+  errors: errorReducers,
+  profile: profileReducers
 });
