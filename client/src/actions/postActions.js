cdf9e68667b0448c3e7539204e7471c626c305a3
DONGVAN <viendong194@gmail.com>
2018-12-02 14:13:44 +0900

    add postItem

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_POSTS,GET_ERRORS,POST_LOADING } from './types.js';
+import {
+  ADD_POST,
+  GET_POSTS,
+  GET_ERRORS,
+  DELETE_POST,
+  POST_LOADING
+} from './types.js';
 
 // add post
 
@@ -21,7 +27,7 @@ export const addPost = (postData) => (dispatch) => {
 // get post
 
 export const getPosts = () => (dispatch) => {
-  dispatch(setPostLoading())
+  dispatch(setPostLoading());
   axios.get('/api/posts').then((res) =>
     dispatch({
       type: GET_POSTS,
@@ -35,10 +41,52 @@ export const getPosts = () => (dispatch) => {
   );
 };
 
+// delete post
+
+export const deletePost = (id) => (dispatch) => {
+  axios.delete(`/api/posts/${id}`).then((res) =>
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    }).catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.respond.data
+      })
+    )
+  );
+};
+
+// add like post
+
+export const addLike = (id) => (dispatch) => {
+  axios.post(`/api/posts/like/${id}`).then((res) =>
+    dispatch(getPosts()).catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.respond.data
+      })
+    )
+  );
+};
+
+// remove like post
+
+export const removeLike = (id) => (dispatch) => {
+  axios.post(`/api/posts/unlike/${id}`).then((res) =>
+    dispatch(getPosts()).catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.respond.data
+      })
+    )
+  );
+};
+
 // set state loading
 
-export const setPostLoading = () =>{
+export const setPostLoading = () => {
   return {
     type: POST_LOADING
-  }
-}
+  };
+};
