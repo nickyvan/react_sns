dfd77328ffa8a1b9df81714c9dbb0d94fc5bf83b
DONGVAN <viendong194@gmail.com>
2018-12-02 11:55:10 +0900

    add post reducers,actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 import authReducers from './authReducers';
 import errorReducers from './errorReducers';
 import profileReducers from './profileReducers';
+import postReducers from './postReducers';
+
 export default combineReducers({
   auth: authReducers,
   errors: errorReducers,
-  profile: profileReducers
+  profile: profileReducers,
+  post: postReducers
 });
